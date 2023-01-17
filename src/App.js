bb8720faddd9df575f0a2780630f45b76b3a43c7
Aydyn Mekhtiev <mehtievaa@gmail.com>
2023-01-17 23:36:27 +0700

    new flow

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import './App.css';
 import Main from './components/Main';
-
+import {BrowserRouter as Router, Routes} from 'react-router-dom';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App">
       <Main/>
+      <Router>
+        <div className={classes.header}>
+          <Header/>
+        </div>
+        <div className={classes.container}>
+          <div className={classes.main}>
+            <div className={classes.router}>
+              <Routes>
+                <Route
+                  path="/main"
+                  element={<StartPage/>}
+                  />
+                <Route
+                  path="/create_notification"                
+                  element={<CreateNotification/>}                
+                  />                          
+                <Route
+                  path="/notifications_list"                  
+                  element={<NotificationList/>}                  
+                  />              
+              </Routes>              
+            </div>
+          </div>
+        </div>
+      </Router>          
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,8 @@ import Menu from './MenuComponent';
 import DishDetail from './DishDetail';
 import { DISHES } from '../shared/dishes';
 import ModalWindow from './Modal/Modal';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 
 
 export default function Main() {
@@ -21,17 +23,17 @@ export default function Main() {
 
   return (
     <div>
-        <Navbar dark color='primary'>
+        {/*<Navbar dark color='primary'>
             <div className='container'>
             <NavbarBrand href='/'>Ristorante con Fusions</NavbarBrand>
             </div>
-        </Navbar>
-        
+          </Navbar>*/}
+        <Header />
         <Menu dishes={dishes} onClick={(dishId) => {onDishSelect(dishId);setShow(true);console.log(dishId)}} />
         <ModalWindow onClose={() => setShow(false)} show={show} >
           <DishDetail dish={dishes.filter((dish) => dish.id === selectedDish)[0]}/>
         </ModalWindow>
-        
+        <Footer /> 
     </div>
   );
 }
