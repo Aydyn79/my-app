f9f0721250f6019849f81a59816eef27a35835ac
Aydyn Mekhtiev <mehtievaa@gmail.com>
2023-01-30 00:04:52 +0700

    redux redaction

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import Main from './components/Main';
 import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './components/redux/configureStore';
 
+const store = ConfigureStore();
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Main/>
-      </div>
-    </BrowserRouter>          
+    <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+    </Provider>         
   
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -10,29 +10,43 @@ import ModalWindow from './Modal/Modal';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
-import { Routes, Route, Navigate, useParams } from 'react-router-dom';
+import { Routes, Route, Navigate, useParams} from 'react-router-dom';
 import Contact from './ContactComponent';
 import AboutUs from './AboutUsComponent';
+import { connect } from 'react-redux';
+import { withRouter } from '../utils/wr';
 
-export default function Main() {
-  const [dishes, setDishes] = useState([]);
-  const [comments, setComments] = useState([]);
-  const [promotions, setPromotions] = useState([]);
-  const [leaders, setLeaders] = useState([]);
-  const [selectedDish, setSelectedDish] = useState(null);
+const mapStateToProps = state => {
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders
+  }
+}
+
+
+const Main = (props) => {
+  // const [dishes, setDishes] = useState([]);
+  // const [comments, setComments] = useState([]);
+  // const [promotions, setPromotions] = useState([]);
+  // const [leaders, setLeaders] = useState([]);
+  // const [selectedDish, setSelectedDish] = useState(null);
   
-  useEffect(() => {
-        setDishes(DISHES);
-        setComments(COMMENTS);
-        setPromotions(PROMOTIONS);
-        setLeaders(LEADERS);
-    }, []);
+  // useEffect(() => {
+  //       setDishes(DISHES);
+  //       setComments(COMMENTS);
+  //       setPromotions(PROMOTIONS);
+  //       setLeaders(LEADERS);
+  //   }, []);
+
+
 
   const DishWithId = () => {
         let id = useParams().dishId
         return(
-              <DishDetail dish={dishes.filter((dish) => dish.id === Number(id))[0]} 
-                comments={comments.filter((comment) => comment.dishId === Number(id))} />
+              <DishDetail dish={props.dishes.filter((dish) => dish.id === Number(id))[0]} 
+                comments={props.comments.filter((comment) => comment.dishId === Number(id))} />
           );
         };
 
@@ -41,14 +55,14 @@ export default function Main() {
         <Header />
         <Routes>
           <Route path='/home' element={<Home 
-          dish={dishes.filter((dish) => dish.featured)[0]}
-          promotion={promotions.filter((promo) => promo.featured)[0]}
-          leader={leaders.filter((leader) => leader.featured)[0]}
+          dish={props.dishes.filter((dish) => dish.featured)[0]}
+          promotion={props.promotions.filter((promo) => promo.featured)[0]}
+          leader={props.leaders.filter((leader) => leader.featured)[0]}
           />} />
-          <Route exact path='/menu' element={<Menu dishes={dishes} />} />
+          <Route exact path='/menu' element={<Menu dishes={props.dishes} />} />
           <Route path='/menu/:dishId' element={<DishWithId />} />
           <Route path='/contactus' element={<Contact />} />
-          <Route path='/aboutus' element={<AboutUs leaders={leaders}/>} />
+          <Route path='/aboutus' element={<AboutUs leaders={props.leaders}/>} />
           <Route path="*" element={<Navigate replace to="/home" />} />
         </Routes>
         <Footer /> 
@@ -57,3 +71,4 @@ export default function Main() {
 }
 
 
+export default withRouter(connect(mapStateToProps)(Main));
